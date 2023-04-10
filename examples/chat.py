a6e99aac598122c3f77199bb2687ce2607ccb888
Peter <pleupi123@gmail.com>
2023-04-10 19:25:47 +0200

    working poc

--- examples/chat.py
+++ examples/chat.py
@@ -1,61 +1,38 @@
-from typing import Optional
-from datetime import datetime
-
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 
 from llmio.assistant import Assistant
 
 
 assistant = Assistant(
     short_description="""
-        You are a Oslo Taxis taxi booking assistant.
+        You are a calculator.
+        Always use the provided commands to calculate things,
+        do not attemt to calculate things yourself.
     """,
-    key="123",
+    key=open("/Users/peterleupi/.creds/openai").read().strip(),
 )
 
 
-@assistant.command(name="SPEAK", description="The message here will be displayed to the user.")
-def speak(message: str):
-    print(message)
+class NumberPair(BaseModel):
+    number1: float
+    number2: float
 
 
-class BookTaxiParams(BaseModel):
-    pickup_location: str = Field(..., description="Pickup location")
-    destination: str = Field(..., description="Destination")
-    number_of_people: int = Field(..., description="Number of people")
-    pickup_time: datetime = Field(..., description="Pickup time")
-    additional_info: Optional[str] = Field(..., description="Any additional info that might be useful for the operator or driver to know about.")
+class Result(BaseModel):
+    result: float
 
 
-class BookTaxiResult(BaseModel):
-    success: bool = Field(..., description="Pickup location")
-    booking_id: Optional[str] = Field(None, description="Unique Booking ID upon successful booking.")
-    message: str = Field("", description="Explanation of result")
+@assistant.command(description="Add two numbers")
+def add(params: NumberPair) -> Result:
+    print(f"Add {params}")
+    return Result(result=params.number1 + params.number2)
 
 
-@assistant.command(name="BOOK_TAXI", description="Execute a taxi booking. Make sure the user has confirmed the details before executing this.")
-def book_taxi(params: BookTaxiParams) -> BookTaxiResult:
-    print(params)
+@assistant.command(description="Multiply two numbers")
+def multiply(params: NumberPair) -> Result:
+    print(f"Multiply {params}")
+    return Result(result=params.number1 * params.number2)
 
 
-class EditBookingParams(BaseModel):
-    booking_id: str = Field(..., description="Booking ID of existing booking.")
-    pickup_location: str = Field(..., description="Change pickup location")
-    destination: str = Field(..., description="Change destination")
-    number_of_people: int = Field(..., description="Change number of people")
-    pickup_time: datetime = Field(..., description="Change pickup time")
-    additional_info: Optional[str] = Field(..., description="Append additional info that might be useful for the operator or driver to know about.")
-
-
-class EditBookingResult(BaseModel):
-    success: bool = Field(..., description="Pickup location")
-    booking_id: Optional[str] = Field(None, description="Unique Booking ID upon successful booking.")
-    message: str = Field("", description="Explanation of result")
-
-
-@assistant.command(name="EDIT_BOOKING", description="Edit an existing taxi booking. Only not-null parameters will be written.")
-def edit_booking(params: EditBookingParams) -> EditBookingResult:
-    print(params)
-
-
-reply = assistant.speak("Hei, jeg trenger en taxi.")
+reply = assistant.speak("How much is ((10 * 2) + (100 * 12345)) * 2?")
+print(reply)
--- llmio/assistant.py
+++ llmio/assistant.py
@@ -1,7 +1,9 @@
 from functools import wraps
-from typing import Any
+from typing import Optional, Literal
 from dataclasses import dataclass
+import jinja2
 
+import pydantic
 import openai
 
 
@@ -14,12 +16,53 @@ ENGINES = {
 @dataclass
 class Command:
     name: str
+    function: callable
     description: str
-    params: Any
+    params: pydantic.BaseModel
+    returns: pydantic.BaseModel
 
+    def explain(self):
+        return jinja2.Template("""
+            Command: {{name}}
+            Description: {{description}}
+            Parameters:
+            | Name | Type | Description |
+            | ---- | ---- | ----------- | \
+            {% for param_name, param_type, param_desc in params %}
+            | {{param_name}} | {{param_type}} | {{param_desc}} | \
+            {% endfor %}
+
+            Returns:
+            | Name | Type | Description |
+            | ---- | ---- | ----------- | \
+            {% for res_name, res_type, res_desc in returns %}
+            | {{res_name}} | {{res_type}} | {{res_desc}} | \
+            {% endfor %}
+
+            Example usage:
+            {{mock_data}}
+        """).render(
+            name=self.name,
+            description=self.description,
+            params=[(key, value["type"], "") for key, value in self.params.schema()["properties"].items()],
+            returns=[(key, value["type"], "") for key, value in self.returns.schema()["properties"].items()],
+            mock_data=self.mock_data().json(),
+        )
+
+    def command_model(self):
+        class Model(pydantic.BaseModel):
+            command: Literal[self.name]
+            params: self.params
+        return Model
+
+    def mock_data(self):
+        from polyfactory.factories.pydantic_factory import ModelFactory
+        class Mocker(ModelFactory):
+            __model__ = self.command_model()
+        return Mocker.build()
 
 class Assistant:
-    def __init__(self, key: str, short_description: str, engine: str = "gpt-3.5-turbo"):
+    def __init__(self, key: str, short_description: str, engine: str = "gpt-4"):
         openai.api_key = key
 
         if engine not in ENGINES:
@@ -35,10 +78,28 @@ class Assistant:
     def description(self) -> str:
         return self.short_description
 
+    def system_prompt(self) -> str:
+        return jinja2.Template("""
+        {{short_description}}
+
+        The following commands can be used.
+        If you intend to execute a command, only write a valid command and nothing else.
+        Do not try to both speak and execute a command at the same time, as it will not be accepted as a command.
+        Also do not try to execute multiple commands at once.
+        You can chain commands, but if so, only execute one command at a time, and then execute the next commands afterward.
+
+        {% for command in commands %}
+        {{command.explain()}}
+        {% endfor %}
+        """).render(
+            short_description=self.short_description,
+            commands=self.commands,
+        )
+
     def _get_system_prompt(self) -> str:
         return {
             "role": "system",
-            "content": self.description
+            "content": self.system_prompt()
         }
 
     @property
@@ -56,26 +117,61 @@ class Assistant:
 
         return result["choices"][0]["message"]["content"]
 
-    def command(self, name: str, description: str):
-        print(f"Creating command {name}")
+    def command(self, description: Optional[str] = None):
         def wrapper(function):
+            assert len(function.__annotations__) == 2
+            print(f"Creating command {function.__name__}")
+
+            print(function.__annotations__)
+
+            assert "return" in function.__annotations__
+            assert issubclass(function.__annotations__["return"], pydantic.BaseModel)
+            input_annotation = [(key, t) for key, t in function.__annotations__.items() if key != "return"]
+            assert issubclass(input_annotation[0][1], pydantic.BaseModel)
+
             @wraps(function)
             def wrapped(*args, **kw):
                 return function(*args, **kw)
 
             self.commands.append(
                 Command(
-                    name=name,
+                    name=function.__name__,
+                    function=wrapped,
                     description=description,
-                    params=[
-                        (name, type)
-                        for name, type in function.__annotations__.items()
-                        if name != "return"
-                    ]
+                    params=input_annotation[0][1],
+                    returns=function.__annotations__["return"]
                 )
             )
             return wrapped
         return wrapper
 
-    def run(self):
-        pass
+
+    def speak(self, message: str, role="user") -> str:
+        self._messages.append({
+            "role": role,
+            "content": message,
+        })
+        result = openai.ChatCompletion.create(
+            model=self.engine,
+            messages=self.history,
+        )
+        content = result["choices"][0]["message"]["content"]
+        self._messages.append({
+            "role": "assistant",
+            "content": content,
+        })
+        print(content)
+
+        for command in self.commands:
+            model = command.command_model()
+            print(model)
+            try:
+                inputs = model.parse_raw(content)
+            except Exception as e:
+                print(e)
+                print("Not valid")
+                continue
+            print("Valid command!")
+            result = command.function(inputs.params)
+            return self.speak(result.json(), role="system")
+        return content
