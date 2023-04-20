80178f94878978fb1d506aa8ff233bb5a81aa144
Peter <pleupi123@gmail.com>
2023-04-20 23:54:33 +0200

    test history

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -6,17 +6,27 @@ from . import utils
 
 
 def test_calculator_1():
-    with utils.mocked_openai_reply("Hello!"):
-        reply, _ = calculator.assistant.speak(
-            "Hi! I am a calculator. How can I help you?"
-        )
-        assert reply == "Hello!"
+    with utils.mocked_openai_reply("Hi! I am a calculator. How can I help you?"):
+        reply, history = calculator.assistant.speak("hi")
+        assert reply == "Hi! I am a calculator. How can I help you?"
 
-    with utils.mocked_openai_replies(
-        [
-            json.dumps({"command": "add", "params": {"num1": 10, "num2": 20}}),
-            "The answer is 30!",
-        ]
-    ):
-        reply, _ = calculator.assistant.speak("calculate 10 + 20")
+    replies = [
+        json.dumps({"command": "add", "params": {"num1": 10, "num2": 20}}),
+        "The answer is 30!",
+    ]
+    with utils.mocked_openai_replies(replies):
+        reply, history = calculator.assistant.speak(
+            "calculate 10 + 20", history=history
+        )
         assert reply == "The answer is 30!"
+        assert history == [
+            {"role": "user", "content": "hi"},
+            {
+                "role": "assistant",
+                "content": "Hi! I am a calculator. How can I help you?",
+            },
+            {"role": "user", "content": "calculate 10 + 20"},
+            {"role": "assistant", "content": replies[0]},
+            {"role": "system", "content": json.dumps({"result": 30.0})},
+            {"role": "assistant", "content": replies[1]},
+        ]
